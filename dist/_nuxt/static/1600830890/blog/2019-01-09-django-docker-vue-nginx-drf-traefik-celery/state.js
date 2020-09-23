add9e9549dad58647acbf5fed0d172fae7830dab
Brian <briancaffey2010@gmail.com>
2020-09-22 23:36:26 -0400

    commited dist files

---
+++ dist/_nuxt/static/1600830890/blog/2019-01-09-django-docker-vue-nginx-drf-traefik-celery/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1600830890",layout:"default",error:null,serverRendered:a,routePath:"\u002Fblog\u002F2019-01-09-django-docker-vue-nginx-drf-traefik-celery",config:{content:{dbHash:"068f3fb5"}},colorMode:{preference:b,value:b,unknown:a}}}(true,"system"));
---
+++ dist/_nuxt/static/1600830890/projects/bucksamonth/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1600830890",layout:"default",error:null,serverRendered:a,routePath:"\u002Fprojects\u002Fbucksamonth",config:{content:{dbHash:"068f3fb5"}},colorMode:{preference:b,value:b,unknown:a}}}(true,"system"));
