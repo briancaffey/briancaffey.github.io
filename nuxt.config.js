c1feb4d1e35d236f00e95f7eafba34ada9dc5110
Brian Caffey <briancaffey2010@gmail.com>
2020-10-18 13:36:58 -0400

    added drift chat widget to site

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,11 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ['~/plugins/disqus', '~/plugins/filters'],
+  plugins: [
+    '~/plugins/disqus',
+    '~/plugins/filters',
+    { src: '~plugins/drift.js', mode: 'client' },
+  ],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
