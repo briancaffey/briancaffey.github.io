f5c67d5419f47e9d1f09f9e7531cb4471fed4478
Brian Caffey <briancaffey2010@gmail.com>
2024-11-09 16:40:23 +0700

    fix(lint): placeholder child elements for x post components

--- components/content/redlm/RedLMDeepDiveVideo.vue
+++ components/content/redlm/RedLMDeepDiveVideo.vue
@@ -1,5 +1,5 @@
 <template>
 
   <!-- <blockquote class="twitter-tweet tw-align-center" data-media-max-width="560"><p lang="en" dir="ltr">Agents of Inference<br>🍸🤵🏼‍♂️⚡️🎥🎬<a href="https://twitter.com/hashtag/NVIDIADevContest?src=hash&amp;ref_src=twsrc%5Etfw">#NVIDIADevContest</a> <a href="https://twitter.com/hashtag/LangChain?src=hash&amp;ref_src=twsrc%5Etfw">#LangChain</a> <a href="https://twitter.com/NVIDIAAIDev?ref_src=twsrc%5Etfw">@NVIDIAAIDev</a> <a href="https://t.co/VT3rgzFbD6">pic.twitter.com/VT3rgzFbD6</a></p>&mdash; Brian Caffey (@briancaffey) <a href="https://twitter.com/briancaffey/status/1802754703207583886?ref_src=twsrc%5Etfw">June 17, 2024</a></blockquote> <component :is="'script'" async src="https://platform.twitter.com/widgets.js" charset="utf-8"/> -->
-
+  <div></div>
 </template>
